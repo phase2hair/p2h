5225c732d87a4424f5e6b8f320156ca2a171742e
phase2hair <139242246+phase2hair@users.noreply.github.com>
2023-07-12 20:18:02 +0100

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,3 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
+
+$route['home/register_new_client'] = 'home/register_new_client';
+$route['home/check_in'] = 'home/check_in';
+$route['admin/dashboard'] = 'admin/dashboard';
