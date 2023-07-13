3eb33f9737706cd092b1064eaa6b5c0a795cf5f8
phase2hair <139242246+phase2hair@users.noreply.github.com>
2023-07-13 09:53:39 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -5,8 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = "http://".$_SERVER['HTTP_HOST'].
 $config['base_url'] = "https://phase2hair.azurewebsites.net/index.php/";
 $config['assets_url'] = "https://phase2hair.azurewebsites.net/";
-
-
+ 
 //str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 $config['index_page'] = '';
